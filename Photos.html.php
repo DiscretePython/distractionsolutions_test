6f5446f6e7095d354f8f24d6a4b074922c5f3531
yakyak25 <32824283+yakyak25@users.noreply.github.com>
2017-10-18 20:08:26 -0300

    Add files via upload
    Set Up Dev space for php and started switching some of the stuff over(navbar).  Updated news.

---
+++ Photos.html.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Distraction Solutions - Photos</title>
+	<link rel="stylesheet" href="./css/Styles.css">
+</head>
+<body class="back_photos">
+	<?php include 'navbar.php';?>
+	<div id="top"></div>
+	<div class="i1"></div>
+	<div class="i1_des"> A scan of an old Combine</div>
+	<div class="i2">
+	<div class="i2_des"> The sleepy russian town of Port Baikal (2012)</div> </div>
+	<div class="i3"></div>
+	<div class="i3_des"> A Twin Otter in Northern Manitoba near Camp Whitney (2012)</div>
+	<div class="i4"></div>
+	<div class="i4_des"> Clouds over Lake Baikal in early winter (2011)</div>
+	<div class="i5"></div>
+	<div class="i5_des"> The view from the top of a grain elevator in Newdale Manitoba (2013)</div>
+	<div class="i6"></div>
+	<div class="i6_des"> A skater on the north end of Lake Baikal. (2012)</div>
+	<div class="i7"></div>
+	<div class="i7_des"> "Don't look down" (2012)</div>		
+	<div class="header_title"> Will's Favorite Photos</div> 
+
+</body>
+</html>
---
+++ alpha/Login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Title</title>
+</head>
+<body>
+<?php 
+echo "Hello world!"; 
+?>
+<?php 
+echo $_POST['username']; 
+?>
+</body>
+</html>
