f34f7d9bb22e1bca5a8bdfefe8bd4b8c55a79312
yakyak25 <32824283+yakyak25@users.noreply.github.com>
2017-10-19 13:28:25 -0300

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,9 +1,9 @@
 <?php
 echo ("<div class=\"navbar\">
-  		<a href=\"index.html.php#home\">Home</a>
-  		<a href=\"index.html.php#news\">News</a>
-		<a href=\"index.html.php#youtube\">Youtube</a>
-	  	<a href=\"index.html.php#contributors\">Contact</a>
-		<a href=\"Photos.html.php#top\">Photos</a>
+  		<a href=\"index.php#home\">Home</a>
+  		<a href=\"index.php#news\">News</a>
+		<a href=\"index.php#youtube\">Youtube</a>
+	  	<a href=\"index.php#contributors\">Contact</a>
+		<a href=\"Photos.php#top\">Photos</a>
 	</div>");
 ?>
