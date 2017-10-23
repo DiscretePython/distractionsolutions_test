ddacb4cc9130010fee62267543e503c499c08f0d
yakyak25 <32824283+yakyak25@users.noreply.github.com>
2017-10-23 16:36:16 -0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php include 'set_cookie.php';?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Distraction Solutions - Homepage</title>
+	<link rel="stylesheet" href="./css/Styles.css">
+</head>
+<body class="back">
+	<?php include 'navbar.php';?>
+	<div id="home"></div>
+	<div class="i-1"></div>
+	<div class="header_title"> er404: Sorry page not found</div> 
+
+
+</body>
+</html>
