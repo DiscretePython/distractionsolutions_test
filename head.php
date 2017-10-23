7ee6a5b40d59cf748f56ea748a722fb72b69fb13
yakyak25 <32824283+yakyak25@users.noreply.github.com>
2017-10-23 18:39:04 -0300

    Add files via upload

---
+++ head.php
@@ -0,0 +1,18 @@
+<?php
+echo ("
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-56897505-2\"></script>
+	<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+
+  		gtag('config', 'UA-56897505-2');
+	</script>
+	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">
+	<title>Distraction Solutions</title>
+	<link rel=\"stylesheet\" href=\"./css/Styles.css\">
+</head>
+");
+?>
