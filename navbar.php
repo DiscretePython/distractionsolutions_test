a793d4635c0e6b8963b3da4e270e507b0c30e7d2
yakyak25 <32824283+yakyak25@users.noreply.github.com>
2017-10-22 21:44:14 -0300

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -6,5 +6,7 @@ echo ("<div class=\"navbar\">
 	  	<a href=\"index.php#contributors\">Contact</a>
 		<a href=\"Photos.php#top\">Photos</a>
 		<a href=\"distractions.php#top\">Resources</a>
+		<a href=\"phpmyadmin/\">SQL Shite</a>
+		
 	</div>");
 ?>
