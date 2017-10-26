10253de3222c4ef334af5a741d208b211fd1b9ce
yakyak25 <willknowling@gmail.com>
2017-10-26 15:56:36 -0300

    Added some styling and content to the resources.

--- distractions.php
+++ distractions.php
@@ -27,44 +27,47 @@
 	<div class="body">
 	<div class="flex-container">
   		<div class="flex-item">Art Links
-			<ul>
-				<li class="list-item"><a href="http://www.mapsmith.net/">MapSmith</a></li>
-				<li class="list-item"><a href="https://color.adobe.com/">Adobe Color Palette</a></li>
-				<li class="list-item"><a href="http://game-icons.net/">Icons</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="http://www.mapsmith.net/">MapSmith</a><br>
+				<a class="list-item" href="https://color.adobe.com/">Adobe Color Palette</a><br>
+				<a class="list-item" href="http://game-icons.net/">Icons</a><br>
+			
 		</div>
   		<div class="flex-item">AI Links
-			<ul>
-				<li class="list-item"><a href="https://www.captionbot.ai/">Caption Bot</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="https://www.captionbot.ai/">Caption Bot</a><br>
+				<a class="list-item" href="https://www.playground.tensorflow.org/">Basic nuaral network</a><br>
+		
 		</div>
   		<div class="flex-item">Web Links Dev
-			<ul>
-				<li class="list-item"><a href="http://shancarter.com/">Shan Carter</a></li>
-				<li class="list-item"><a href="http://dwtkns.com/portfolio/">Derek Watkins </a></li>
-				<li class="list-item"><a href="http://form.guide/php-form/php-login-form.html">Basic php Login Form</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="http://shancarter.com/">Shan Carter</a><br>
+				<a class="list-item" href="http://dwtkns.com/portfolio/">Derek Watkins </a><br>
+				<a class="list-item" href="http://form.guide/php-form/php-login-form.html">Basic php Login Form</a><br>
+				<a class="list-item" href="http://www.fileformat.info/info/unicode/block/egyptian_hieroglyphs/utf8test.htm">UTF Hyroglifics</a><br>
+				
+			
 		</div>
 		<div class="flex-item">Comdic Relief
-			<ul>
-				<li class="list-item"><a href="https://www.reddit.com/r/LyricsSpoiledByHitler/top/">r\LyricsSpoiledByHitler</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="https://www.reddit.com/r/LyricsSpoiledByHitler/top/">r\LyricsSpoiledByHitler</a>
+			
 		</div>
 		<div class="flex-item">Map Data Links
-			<ul>
-				<li class="list-item"><a href="http://www.naturalearthdata.com/">Natural Earth Data</a></li>
-				<li class="list-item"><a href="https://www.google.com/maps/d/viewer?mid=1oaV0uhk7i3q4oHJsD8WMuOvCgB0&hl=en_US">Local Dive Sites</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="http://www.naturalearthdata.com/">Natural Earth Data</a><br>
+				<a class="list-item" href="https://www.google.com/maps/d/viewer?mid=1oaV0uhk7i3q4oHJsD8WMuOvCgB0&hl=en_US">Local Dive Sites</a>
+			
 		</div> 
 		<div class="flex-item">Weather
-			<ul>
-				<liclass="list-item"><a href="http://weather.gc.ca/marine/weatherConditions-currentConditions_e.html?mapID=15&siteID=14100&stationID=44258">Maritime Weather</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="http://weather.gc.ca/marine/weatherConditions-currentConditions_e.html?mapID=15&siteID=14100&stationID=44258">Maritime Weather</a>
+			
 		</div> 
 		<div class="flex-item">Schematics
-			<ul>
-				<li class="list-item"><a href="https://www.fs.fed.us/database/acad/lt/">US Fire Watch Towers</a></li>
-			</ul>
+			<br>
+				<a class="list-item" href="https://www.fs.fed.us/database/acad/lt/">US Fire Watch Towers</a>
+			
 		</div> 
 	</div>
 	 
