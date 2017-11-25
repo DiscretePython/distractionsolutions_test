18a0f0bbfbb1d24ee486e0201b097e22ac3587e2
root <justin.j.hiltz@hotmail.com>
2017-11-25 06:07:41 +0000

    Adding login page for project

---
+++ project/login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+  <title>Branch Login</title>
+  <div id="login">
+    <form method="post" action="">
+      <h2>Login <small>Enter Branch Credentials</small></h2>
+      <p>
+	<label for="branch_id">Branch ID: </label>
+	<input type="text" name="username" />
+      </p>
+
+      <p>
+	<label for="pwd">Password: </label>
+	<input type="text" name="pwd" />
+      </p>
+
+      <p>
+	<input type="submit" id="submit" value="Login" name="submit" />
+      </p>
+    </form>
+  </div>
+</html>
